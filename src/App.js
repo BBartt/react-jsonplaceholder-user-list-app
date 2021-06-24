b9126d9df0d2dd76053068edd5930e9ced3f87d0
Bartosz <bartosz.polakowski@escola.pl>
2021-06-24 17:42:31 +0200

    Add form, input etc (prepare structure).

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function App() {
+  const [userName, setUserName] = useState("");
+  
   return (
-    <div>
-      test
+    <div className="content">
+      <h1>Users list</h1>
+      <form>
+        <input
+          className="input"
+          placeholder="Search by user name..."
+          value={userName}
+          onChange={({ target: {value} })=> setUserName(value)}
+        />
+
+        <ol className="ol">
+          <li className="li">
+            Leanne Graham {" "}
+            <span className="userName">@{"bret"}</span>
+          </li>
+
+          <li className="li">
+            Ervin Howell {" "}
+            <span className="userName">@{'Antonette'}</span>
+          </li>
+
+          <li className="li">
+            Clementine Bauch {" "}
+            <span className="userName">@{'Samantha'}</span>
+          </li>
+        </ol>
+      </form>
+
     </div>
   );
 }
