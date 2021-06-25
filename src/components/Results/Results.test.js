c635ecf1c7393e39c0592766a06b56d1d0a82c60
bbartt <bartek2@onet.eu>
2021-06-25 12:19:14 +0200

    Remve unused import from Results test file

--- src/components/Results/Results.test.js
+++ src/components/Results/Results.test.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 
 import { cleanup, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import userEvent from "@testing-library/user-event";
 import Results from ".";
 
 afterEach(cleanup);
