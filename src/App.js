571cbc6f77a0ab997cbae34edb5e59e4d7b6b411
Bartosz <bartosz.polakowski@escola.pl>
2021-06-25 09:59:50 +0200

    Fix destructuring length property

--- src/App.js
+++ src/App.js
@@ -1,76 +1,71 @@
-import React, {useState, useEffect} from 'react';
-import Results from './components/Results';
-import { BASE_URL } from './constants';
-
+import React, { useState, useEffect } from "react";
+import Results from "./components/Results";
+import { BASE_URL } from "./constants";
 
 const App = () => {
   const [userName, setUserName] = useState("");
 
   const [users, setUsers] = useState([]);
-  
+
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(undefined);
 
   useEffect(() => {
-
     const fetchUsers = async () => {
       setIsLoading(true);
       setError("");
-      
+
       await fetch(BASE_URL)
-            .then( res => {
-              if(!res.ok) return setError("Server responds with error: " + res.status);
+        .then((res) => {
+          if (!res.ok)
+            return setError("Server responds with error: " + res.status);
 
-              return res.json();
-            })
-            .then(data => {
-              const { length } = data;
-              
-              setIsLoading(false);
-              !length > 0 && setError("No data");
-              setUsers( length > 0 ? data : [] );
-            })
-            .catch(error => {
-              console.error(`catch error`, error);
+          return res.json();
+        })
+        .then((data) => {
+          setIsLoading(false);
+          !data?.length > 0 && setError("No data");
+          setUsers(data?.length > 0 ? data : []);
+        })
+        .catch((error) => {
+          console.error(`catch error`, error);
 
-              setIsLoading(false);
-              setError(error.message);
-            });
-    }
+          setIsLoading(false);
+          setError(error.message);
+        });
+    };
 
     fetchUsers();
-    
   }, []);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    if(!userName.length) return setError("Field can not be empty.");
-    
+    if (!userName.length) return setError("Field can not be empty.");
+
     setIsLoading(true);
     setUserName("");
     setError("");
 
     return await fetch(`${BASE_URL}?name=${userName}`)
-                .then( res => {
-                  if(!res.ok) return setError("Server responds with error: " + res.status);
+      .then((res) => {
+        if (!res.ok)
+          return setError("Server responds with error: " + res.status);
 
-                  return res.json();
-                })
-                .then(data => {
-                  const { length } = data;
-                  
-                  setIsLoading(false);
-                  !length > 0 && setError("There is no user like this");
-                  setUsers( length > 0 ? data : [] );
-                })
-                .catch(error => {
-                  console.error(`catch error`, error);
+        return res.json();
+      })
+      .then((data) => {
+        setIsLoading(false);
+        !data?.length > 0 && setError("There is no user like this");
+        setUsers(data?.length > 0 ? data : []);
+      })
+      .catch((error) => {
+        console.error(`catch error`, error);
 
-                  setIsLoading(false);
-                  setError(error.message);
-                });
-  }
+        setIsLoading(false);
+        setError(error.message);
+      });
+  };
 
   return (
     <div className="content">
@@ -81,14 +76,13 @@ const App = () => {
           className="input"
           placeholder="Search by user name..."
           value={userName}
-          onChange={({ target: {value} })=> setUserName(value)}
+          onChange={({ target: { value } }) => setUserName(value)}
         />
       </form>
 
       <Results error={error} isLoading={isLoading} users={users} />
-
     </div>
   );
-}
+};
 
 export default App;
