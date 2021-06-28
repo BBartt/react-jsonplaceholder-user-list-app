69866b81137bcaec42522090104eb46c49546586
bbartt <bartek2@onet.eu>
2021-06-28 21:17:22 +0200

    Add async and await to fetchData function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,10 @@
-export const fetchData = (url, onS, onF) => {
-  fetch(url)
+export const fetchData = async (url, onS, onF) => {
+  await fetch(url)
     .then((res) => {
       if (!res.ok) throw new Error("Server responds with error: " + res.status);
 
       return res.json();
     })
-    .then((res) => onS(res))
+    .then((data) => onS(data))
     .catch((err) => onF(err));
 };
